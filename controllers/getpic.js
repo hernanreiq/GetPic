dcb2487b686873c6977b118b4a9ea2b56ff31227
Reiq.Laptop <reiqvideos@gmail.com>
2021-08-13 20:46:40 -0400

    Guardado de imagen correcto y publicación de imágenes en el index

--- controllers/getpic.js
+++ controllers/getpic.js
@@ -3,19 +3,20 @@
 const PicModel = require('../models/getpic');
 
 var controller = {
-    home: function(req, res){
-        res.status(200).render('index');
+    home: async function(req, res){
+        const images = await PicModel.find();
+        res.render('index', {images});
     },
     upload: function(req, res){
         res.status(200).render('upload');
     },
-    imageUploaded: function(req, res){
-        console.log(req.body)
+    imageUploaded: async function (req, res){
         const image = new PicModel();
         image.title = req.body.title;
         image.description = req.body.description;
         image.image = req.file.filename;
         image.path = '/img/uploads/' + req.file.filename;
+        await image.save();
         res.status(200).send({message: image});
     },
     delete: function(req, res){
